72a43eceafd431768f29c854df2248543373fb50
YesManKablam <G00301273@gmit.ie>
2015-12-08 20:31:17 +0000

    Adding workshop files

---
+++ src/ie/gmit/sw/client/Client.java
@@ -0,0 +1,18 @@
+package ie.gmit.sw.client;
+
+import java.rmi.Naming;
+
+import ie.gmit.sw.VigenereBreaker;
+
+public class Client {
+	
+	
+	public static void main(String[] args) throws Exception {
+		/*
+		 * This stuff needs to be in your tomcat app
+		 */
+		VigenereBreaker vb = (VigenereBreaker) Naming.lookup("///cypher-service");
+		
+		String result = vb.decrypt("asldknasldjn", 7);
+	}
+}
